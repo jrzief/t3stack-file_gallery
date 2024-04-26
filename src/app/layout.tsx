4481913ab0e20082fc50d34db881370899a1ffb8
Joel <joelrzief@gmail.com>
2024-04-26 15:11:09 -0400

    Uploadthing added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
+
 import { ClerkProvider } from '@clerk/nextjs';
 
 import { Inter } from "next/font/google";
 import TopNav from "./_components/topnav";
+//import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+//import { extractRouterConfig } from "uploadthing/server";
 
 const inter = Inter({
   subsets: ["latin"],
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
  
 //const isDashboardRoute = createRouteMatcher(['/dashboard(.*)']);
-const isAdminRoute = createRouteMatcher(['/admin(.*)']);
+//const isAdminRoute = createRouteMatcher(['/admin(.*)']);
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
  
 export default clerkMiddleware((auth, req) => {
   // Restrict admin route to users with specific role
-  if (isAdminRoute(req)) auth().protect({ role: 'org:admin' });
+ // if (isAdminRoute(req)) auth().protect({ role: 'org:admin' });
  
   // Restrict dashboard routes to signed in users
   if (isProtectedRoute(req)) auth().protect();
